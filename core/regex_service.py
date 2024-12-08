eacf39c4c5500453fe0771836e2e9e7b2da6868c
KRackL1n <karlov2006@bk.ru>
2024-12-08 23:12:37 +0300

    Update regex_service.py

--- core/regex_service.py
+++ core/regex_service.py
@@ -18,10 +18,24 @@ class RegexService:
         return re.findall(PATTERN, text)
     
     def get_snils_in_file(self):
-        pass
+        result_data = self.result_repository.get_all()
+        result_str = ", ".join([str(asdict(result)) for result in result_data])
+        snils = self._find_snils(result_str)
+        return Result(link="jsonFile", snils=snils)
 
     def get_snils_in_web(self, url: str):
-        pass
+        try:
+            response = get(url)
+            if response.status_code == 200:
+                snils = self._find_snils(response.text)
+                self.result_repository.add(Result(link=url, snils=snils))
+                return Result(link=url, snils=snils)
+            else:
+                print(f"Error: Unable to fetch URL {url}, Status Code: {response.status_code}")
+                return Result(link=url, snils=[])
+        except RequestException as e:
+            print(f"Error: {e}")
+            return Result(link=url, snils=[])
 
     def get_snils_in_text(self, text: str = "") -> List[str]:
         snils = self._find_snils(text)
